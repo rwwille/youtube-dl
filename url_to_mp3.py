058039bc5854953e8b16d977cd392e227bcd71af
Weston <rwwille@gmail.com>
2022-04-08 12:09:49 -0700

    Add file

---
+++ url_to_mp3.py
@@ -0,0 +1,38 @@
+# Enter the YouTube URL to get an mp3 file as output
+
+from __future__ import unicode_literals
+import youtube_dl
+
+url = input('Enter YouTube URL- ')
+
+print('Processing...')
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting...')
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
